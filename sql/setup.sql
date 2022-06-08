07b17ff0a5d67c6151c55fdaabb1c715ad40cd08
BrendenSeifried <brendenseifried@gmail.com>
2022-06-08 15:50:04 -0700

    got bulls to show up as first team in beekeeper

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,11 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+Drop table if exists teams;
+
+CREATE table teams (
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR NOT NULL
+    
+);
+
+INSERT INTO teams (name) VALUES ('Bulls');
