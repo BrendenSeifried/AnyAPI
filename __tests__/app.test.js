3ae197be25e0b2476b8f43bcdf4227f5c21427d2
BrendenSeifried <brendenseifried@gmail.com>
2022-06-08 16:52:19 -0700

    both tests passed did not know row in modules is not a booger

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,12 +3,29 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+const { teams } = require('../data/nbateams');
+
+describe('nba test one', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('listed NBA teams with id and name', async () => {
+    const test = await request(app).get('/teams');
+    const result = teams.map((team) => {
+      return { id: team.id, name: team.name };
+    });
+    expect(test.body).toEqual(result);
+  });
+
+  it('checking for one team (Celtics)', async () => {
+    const resp = await request(app).get('/teams/4');
+    const teestingTeamFour = {
+      id: '4',
+      name: 'Celtics',
+      founded: 1946,
+      founder: 'Walter A. Brown',
+    };
+    expect(resp.body).toEqual(teestingTeamFour);
   });
   afterAll(() => {
     pool.end();
